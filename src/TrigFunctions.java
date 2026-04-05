5249005f0ae4de52669d864aaad5dfe2c68cf456
Amruthakrishnan123 <amruthakrishnan936@gmail.com>
2026-04-05 12:35:08 +0530

    exp9done

---
+++ src/TrigFunctions.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class TrigFunctions {
+
+    // Method to calculate sine
+    public static double sine(double angle) {
+        double radians = Math.toRadians(angle);
+        return Math.sin(radians);
+    }
+
+    // Method to calculate cosine
+    public static double cosine(double angle) {
+        double radians = Math.toRadians(angle);
+        return Math.cos(radians);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double angle = sc.nextDouble();
+
+        System.out.println("Sine: " + sine(angle));
+        System.out.println("Cosine: " + cosine(angle));
+
+        sc.close();
+    }
+}
